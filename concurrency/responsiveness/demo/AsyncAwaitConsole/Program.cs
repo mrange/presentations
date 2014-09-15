042289df96200eb6c754f53fa9e78656a9b7ebe1
mrange <marten_range@hotmail.com>
2014-09-15 21:02:23 +0200

    Added Async sample

---
+++ concurrency/responsiveness/demo/AsyncAwaitConsole/Program.cs
@@ -0,0 +1,176 @@
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A
+// copy of the license can be found in the License.html file at the root of this distribution.
+// If you cannot locate the  Microsoft Public License, please send an email to
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+using System;
+using System.Globalization;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace AsyncAwait
+{
+    static class Program
+    {
+        delegate SynchronizationContext SynchronizationContextDelegate ();
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            SynchronizationContextDelegate defaultContext       = () => null;
+            SynchronizationContextDelegate windowsFormsContext  = () => new WindowsFormsSynchronizationContext ();
+            SynchronizationContextDelegate dispatcherContext    = () => new DispatcherSynchronizationContext ();
+
+            Info ("Starting test run...");
+
+            // Starting test runs with different variants of
+            // synchronization contexts and continueOnCapturedContext
+
+            RunTestCase (defaultContext     , continueOnCapturedContext:true    );
+            RunTestCase (defaultContext     , continueOnCapturedContext:false   );
+            RunTestCase (windowsFormsContext, continueOnCapturedContext:true    );
+            RunTestCase (windowsFormsContext, continueOnCapturedContext:false   );
+            RunTestCase (dispatcherContext  , continueOnCapturedContext:true    );
+            RunTestCase (dispatcherContext  , continueOnCapturedContext:false   );
+
+            Info ("Test run done");
+        }
+
+        static void RunTestCase (SynchronizationContextDelegate contextCreator, bool continueOnCapturedContext)
+        {
+            var thread =
+                new Thread (() => TestCase (contextCreator, continueOnCapturedContext))
+                {
+                    Name                = "Test case"   ,
+                    IsBackground        = true          ,
+                };
+
+            thread.SetApartmentState (ApartmentState.STA);
+
+            thread.Start ();
+
+            var completed = thread.Join (TimeSpan.FromSeconds (1));
+            if (!completed)
+            {
+                Error ("Detected dead-lock");
+                thread.Interrupt ();
+            }
+
+        }
+
+        static void TestCase (SynchronizationContextDelegate contextCreator, bool continueOnCapturedContext)
+        {
+            var previous = SynchronizationContext.Current;
+
+            var context = contextCreator ();
+            SynchronizationContext.SetSynchronizationContext (context);
+
+            var description = context != null ? context.GetType ().Name : "Null" ;
+            Info (
+                "ContinueOnCapturedContext={0} and context={1}".FormatWith (
+                    continueOnCapturedContext   ,
+                    description                 ));
+
+            try
+            {
+                var task = ReadTextTask (continueOnCapturedContext);
+
+                // Awaiting a result on async task in this way may cause a dead-lock
+                // depending on the synchronization context and continueOnCapturedContext
+                var text = task.Result;
+            }
+            catch (Exception exc)
+            {
+                Info ("Thread threw exception: {0}".FormatWith (exc.Message));
+            }
+            finally
+            {
+                var disposable = context as IDisposable;
+                if (disposable != null)
+                {
+                    try
+                    {
+                        disposable.Dispose ();
+                    }
+                    catch (Exception exc)
+                    {
+                        Error ("Caught exception during dispose: {0}", exc.Message);
+                    }
+                }
+
+                SynchronizationContext.SetSynchronizationContext (null);
+            }
+
+        }
+
+        static async Task<string> ReadStreamAsync (string fileName, bool continueOnCapturedContext)
+        {
+            using (var sr = new StreamReader(fileName))
+            {
+                var result = await sr.ReadToEndAsync().ConfigureAwait (continueOnCapturedContext);
+                return result;
+            }
+        }
+
+        static int readingFiles = 0;
+
+        static async Task<string> ReadTextTask (bool continueOnCapturedContext)
+        {
+            var before = Thread.CurrentThread.ManagedThreadId;
+
+            // Tracks how many files are being read concurrently
+            // If the continuation is run by a different thread this can cause race conditions
+            ++readingFiles;
+
+            var text = await ReadStreamAsync ("SomeText.txt", continueOnCapturedContext).ConfigureAwait (continueOnCapturedContext);
+
+            --readingFiles;
+
+            var after = Thread.CurrentThread.ManagedThreadId;
+            if (before != after)
+            {
+                Error ("Race condition detected");
+            }
+
+            return text;
+        }
+
+        static object consoleLock = new object ();
+
+        static void Info (string message, [CallerMemberName] string memberName = null)
+        {
+            lock (consoleLock)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine ("{0}: {1}", memberName, message);
+            }
+        }
+
+        static void Error (string message, [CallerMemberName] string memberName = null)
+        {
+            lock (consoleLock)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine ("{0}: {1}", memberName, message);
+            }
+        }
+
+        static string FormatWith (this string format, params object[] args)
+        {
+            return string.Format (CultureInfo.InvariantCulture, format, args);
+        }
+    }
+}
---
+++ concurrency/responsiveness/demo/AsyncAwaitSample/Program.cs
@@ -0,0 +1,205 @@
+﻿// ----------------------------------------------------------------------------------------------
+// Copyright (c) Mårten Rånge.
+// ----------------------------------------------------------------------------------------------
+// This source code is subject to terms and conditions of the Microsoft Public License. A
+// copy of the license can be found in the License.html file at the root of this distribution.
+// If you cannot locate the  Microsoft Public License, please send an email to
+// dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound
+//  by the terms of the Microsoft Public License.
+// ----------------------------------------------------------------------------------------------
+// You must not remove this notice, or any other, from this software.
+// ----------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace AsyncAwaitSample
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main (string[] args)
+        {
+            //SynchronizationContext.SetSynchronizationContext (new SynchronizationContext ());
+            //SynchronizationContext.SetSynchronizationContext (new DispatcherSynchronizationContext ());
+            //SynchronizationContext.SetSynchronizationContext (new WindowsFormsSynchronizationContext ());
+
+            TestCase1.Run ();
+            //TestCase2.Run ();
+            //TestCase3.Run ();
+        }
+
+        static class TestCase1 
+        {
+            public static void Run ()
+            {
+                Console.WriteLine ("Running TestCase1...");
+
+                var readTask = ReadSomeTextAsync ("SomeText.txt");
+
+                var text = readTask.Result;
+
+                Console.WriteLine ("TestCase1 done");
+            }
+
+            static async Task<string> ReadSomeTextAsync (string fileName)
+            {    
+                using (var sr = new StreamReader (fileName))    
+                {        
+                    var result = await sr.ReadToEndAsync();        
+                    return result;    
+                }
+            }
+        }
+
+        static class TestCase2
+        {
+            static int m_readingFiles;
+
+            public static void Run ()
+            {
+                Console.WriteLine ("Running TestCase2...");
+
+                var tasks = 
+                    Enumerable
+                        .Range (0, 1000)
+                        .Select (i => ReadSomeTextAsync ("SomeText.txt"))
+                        .ToArray ()
+                        ;
+
+                while (!tasks.All (t => t.IsCompleted))
+                {
+                    Application.DoEvents ();
+                    Thread.Sleep (10);
+                }
+
+
+                Console.WriteLine ("m_readingFiles should be 0, but is {0}", m_readingFiles);
+                Console.WriteLine ("TestCase2 done");
+            }
+
+            static async Task<string> ReadSomeTextAsync (string fileName)
+            {    
+                ++m_readingFiles;
+                var result = await ReadFileAsync (fileName);
+                --m_readingFiles;
+                return result;    
+            }
+
+            static async Task<string> ReadFileAsync (string fileName)
+            {
+                await Task.Delay(1);
+                return "Testing";
+            }
+        }
+
+        static class TestCase3
+        {
+            public static void Run ()
+            {
+                Console.WriteLine ("Running TestCase3...");
+
+                var readTask = ReadSomeTextAsync ("SomeText.txt");
+
+                readTask.WaitForTask ();
+
+                Console.WriteLine ("TestCase3 done");
+            }
+
+            static async Task<string> ReadSomeTextAsync (string fileName)
+            {    
+                TraceThreadId ();
+                using (var sr = new StreamReader (fileName))    
+                {        
+                    var result = await sr.ReadToEndAsync();        
+                    TraceThreadId ();
+                    return result;    
+                }
+            }
+
+            static void TraceThreadId()
+            {
+                Console.WriteLine ("Thread Id: {0}", Thread.CurrentThread.ManagedThreadId);
+            }
+        }
+
+        static class TestCase4
+        {
+            static async Task<string[][]> ReadCsvAsync (string fileName)
+            {
+               var result = new List<string[]> ();
+ 
+                using (var sr = new StreamReader (fileName))    
+                {        
+                    var header = await sr.ReadLineAsync ();
+                    if (header != null)
+                    {
+                        var headers = header.Split('\t');
+                        result.Add (headers);
+
+                        string line;
+
+                        while ((line = await sr.ReadLineAsync ()) != null)
+                        {
+                            var fields = line.Split('\t');
+                            if (fields.Length == headers.Length)
+                            {
+                                result.Add (fields);
+                            }
+                        }
+                    }
+
+                }
+
+                return result.ToArray ();
+            }
+
+            static string[][] ReadCsv (string fileName)
+            {
+               var result = new List<string[]> ();
+ 
+                using (var sr = new StreamReader (fileName))    
+                {        
+                    var header = sr.ReadLine ();
+                    if (header != null)
+                    {
+                        var headers = header.Split('\t');
+                        result.Add (headers);
+
+                        string line;
+
+                        while ((line = sr.ReadLine ()) != null)
+                        {
+                            var fields = line.Split('\t');
+                            if (fields.Length == headers.Length)
+                            {
+                                result.Add (fields);
+                            }
+                        }
+                    }
+
+                }
+
+                return result.ToArray ();
+            }
+
+        }
+
+        public static void WaitForTask (this Task task)
+        {
+            while (!task.IsCompleted)
+            {
+                Application.DoEvents ();
+                Thread.Sleep (10);
+            }
+        }
+
+
+    }
+}
