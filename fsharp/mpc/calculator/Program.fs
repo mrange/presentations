11b162d9d59311f296b0c6d76e8f43e9071f7182
mrange <marten_range@hotmail.com>
2015-07-14 15:17:12 +0200

    Improved parser

--- fsharp/mpc/calculator/Program.fs
+++ fsharp/mpc/calculator/Program.fs
@@ -12,86 +12,91 @@
 open System
 open System.Text
 
+module CalculatorModule =
+  open FParsec.Primitives
+  open FParsec.CharParsers
+
+  type UserState  = unit
+  type Parser<'t> = Parser<'t, UserState>
+
+  type BinaryOp =
+    | Add
+    | Subtract
+    | Multiply
+    | Divide
+
+  type AST =
+    | Integer   of int
+    | Variable  of string
+    | Binary    of AST*BinaryOp*AST
+
+  let rec eval (lookup : string -> int) ast =
+    match ast with
+    | Integer i       -> i
+    | Variable v      -> lookup v
+    | Binary (l,op,r) ->
+      let fop=
+        match op with
+        | Add       -> ( + )
+        | Subtract  -> ( - )
+        | Multiply  -> ( * )
+        | Divide    -> ( / )
+
+      let lv = eval lookup l
+      let rv = eval lookup r
+
+      fop lv rv
+
+  let toString ast =
+    let sb = StringBuilder ()
+
+    let rec str ast =
+      match ast with
+      | Integer i       -> ignore <| sb.Append i
+      | Variable v      -> ignore <| sb.Append v
+      | Binary (l,op,r) ->
+        let sop=
+          match op with
+          | Add       -> " + "
+          | Subtract  -> " - "
+          | Multiply  -> " * "
+          | Divide    -> " / "
+
+        ignore <| sb.Append '('
+        str l
+        ignore <| sb.Append sop
+        str r
+        ignore <| sb.Append ')'
+
+    str ast
+
+    sb.ToString ()
+
+  let charToOp = function
+      | '+' -> Add
+      | '-' -> Subtract
+      | '*' -> Multiply
+      | '/' -> Divide
+      | op -> failwith "Unexpected op : %A" op
+
+
+  let pexpr, prexpr     = createParserForwardedToRef<AST, unit> ()
+  let ptoken ch         = skipChar ch .>> spaces
+  let pcharToBinary ops = anyOf ops .>> spaces |>> fun ch -> fun l r -> Binary (l, charToOp ch, r)
+
+  let pint              = pint32 |>> Integer
+  let pvar              = many1Satisfy2L Char.IsLetter Char.IsLetterOrDigit "variable" |>> Variable
+  let psub              = between (ptoken '(') (ptoken ')') pexpr
+  let pterm             = choice [pvar; pint; psub] .>> spaces
+  let p0                = chainl1 pterm (pcharToBinary "*/")
+  let p1                = chainl1 p0 (pcharToBinary "+-")
+  do prexpr             := p1
+
+  let pcalc: Parser<AST>= spaces >>. pexpr .>> eof
+
 open FParsec.Primitives
 open FParsec.CharParsers
-
-type UserState  = unit
-type Parser<'t> = Parser<'t, UserState>
-
-type BinaryOp =
-  | Add
-  | Subtract
-  | Multiply
-  | Divide
-
-type AST =
-  | Integer   of int
-  | Variable  of string
-  | Binary    of AST*BinaryOp*AST
-
-let rec eval (lookup : string -> int) ast =
-  match ast with
-  | Integer i       -> i
-  | Variable v      -> lookup v
-  | Binary (l,op,r) ->
-    let fop=
-      match op with
-      | Add       -> ( + )
-      | Subtract  -> ( - )
-      | Multiply  -> ( * )
-      | Divide    -> ( / )
-
-    let lv = eval lookup l
-    let rv = eval lookup r
-
-    fop lv rv
-
-let toString ast =
-  let sb = StringBuilder ()
-
-  let rec str ast =
-    match ast with
-    | Integer i       -> ignore <| sb.Append i
-    | Variable v      -> ignore <| sb.Append v
-    | Binary (l,op,r) ->
-      let sop=
-        match op with
-        | Add       -> " + "
-        | Subtract  -> " - "
-        | Multiply  -> " * "
-        | Divide    -> " / "
-
-      ignore <| sb.Append '('
-      str l
-      ignore <| sb.Append sop
-      str r
-      ignore <| sb.Append ')'
-
-  str ast
-
-  sb.ToString ()
-
-let charToOp = function
-    | '+' -> Add
-    | '-' -> Subtract
-    | '*' -> Multiply
-    | '/' -> Divide
-    | op -> failwith "Unexpected op : %A" op
-
-
-let pexpr, prexpr     = createParserForwardedToRef<AST, unit> ()
-let ptoken ch         = skipChar ch .>> spaces
-let pcharToBinary ops = anyOf ops .>> spaces |>> fun ch -> fun l r -> Binary (l, charToOp ch, r)
-
-let pint              = pint32 |>> Integer
-let pvar              = many1Satisfy2L Char.IsLetter Char.IsLetterOrDigit "variable" |>> Variable
-let psub              = between (ptoken '(') (ptoken ')') pexpr
-let pterm             = choice [pvar; pint; psub] .>> spaces
-let p0                = chainl1 pterm (pcharToBinary "*/")
-let p1                = chainl1 p0 (pcharToBinary "+-")
-do prexpr             := p1
-
-let pcalc: Parser<AST>= spaces >>. pexpr .>> eof
+open CalculatorModule
 
 [<EntryPoint>]
 let main argv =
