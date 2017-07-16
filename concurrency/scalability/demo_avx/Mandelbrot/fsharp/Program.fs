da44928b1c87438c57a1e359f1bd512d99b92ef8
mrange <marten_range@hotmail.com>
2017-07-16 12:19:58 +0200

    More results

--- concurrency/scalability/demo_avx/Mandelbrot/fsharp/Program.fs
+++ concurrency/scalability/demo_avx/Mandelbrot/fsharp/Program.fs
@@ -251,7 +251,7 @@ let main argv =
   //  Can be viewed using: http://paulcuth.me.uk/netpbm-viewer/
   do
 //    let fs = Console.OpenStandardOutput()
-    use fs = System.IO.File.Create "mandelbrot_fsharp2.pbm"
+    use fs = System.IO.File.Create "mandelbrot_fsharp.pbm"
     use ss = new System.IO.StreamWriter (fs)
     ss.Write (sprintf "P4\n%d %d\n" dim dim)
     ss.Flush ()
