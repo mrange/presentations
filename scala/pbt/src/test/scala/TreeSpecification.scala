a255af1560e331a0a1401e4343501c2ba081fbed
mrange <marten_range@hotmail.com>
2018-02-28 21:49:23 +0100

    Removed tests

--- scala/pbt/src/test/scala/TreeSpecification.scala
+++
@@ -1,8 +0,0 @@
-import org.scalacheck.Prop.forAll
-import org.scalacheck.Properties
-
-class TreeSpecification extends Properties("Tree") {
-  property("startsWith") = forAll { (a: String, b: String) =>
-    ("2" + a+b+"1").startsWith(a)
-  }
-}
